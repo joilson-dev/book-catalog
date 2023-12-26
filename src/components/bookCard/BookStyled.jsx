86f64db1839272c0f0f5abfd6ab74302db438a8a
Joilson Rodrigues <joilson1010silva@gmail.com>
2023-12-25 23:31:14 -0300

    Update BookStyled.jsx

--- src/components/bookCard/BookStyled.jsx
+++ src/components/bookCard/BookStyled.jsx
@@ -3,18 +3,13 @@ import styled from "styled-components";
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
   align-items: center;
-
   border-radius: 0.75rem;
   width: 340px;
   background: #f7f2fa;
 `;
 
 const Image = styled.img`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   height: 360px;
 `;
 
@@ -22,15 +17,12 @@ const Info = styled.div`
   display: flex;
   flex-direction: column;
   padding: 16px;
-  /* align-items: center;  */
 `;
 
 const Title = styled.h2`
   display: flex;
-  /* flex: 1; */
   color: #1d1b20;
   text-align: center;
-  align-items: stretch;
 
   font-family: Roboto;
   font-size: 16px;
