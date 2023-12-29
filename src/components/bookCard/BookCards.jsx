c3b2deb61792ef37c0ab7a9b9a83466c99bc8b7f
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2023-12-29 20:22:50 -0300

    segunda forma de representar o src

--- src/components/bookCard/BookCards.jsx
+++ src/components/bookCard/BookCards.jsx
@@ -2,8 +2,8 @@ import { BookContainer } from "./BookCardtStyled";
 import Book from "./Book";
 import PropTypes from "prop-types";
 
-import { books } from "../../Datas.js";
-1;
+// import { books } from "../../Datas.js";
+import { books } from "/@/Datas.js";
 
 function BookCards({ searchTerm }) {
   const filteredBooks = books.filter((thebook) =>
